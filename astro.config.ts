f26406b8243626e5e2215efaf1296a4431de0a1d
e.muhamethanov <eldarmu.1m@gmail.com>
2024-10-31 14:59:27 +0300

    Подготовка к деплою

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,22 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
+const SERVER_PORT = 3000
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+const LIVE_URL = 'https://EldarMuhamethanov.github.io'
+
+const SCRIPT = process.env.npm_lifecycle_script || ''
+const isBuild = SCRIPT.includes('astro build')
+let BASE_URL = LOCALHOST_URL
+if (isBuild) {
+  BASE_URL = LIVE_URL
+}
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
+  base: '/my_portfolio',
   integrations: [
     react(),
     tailwind({
