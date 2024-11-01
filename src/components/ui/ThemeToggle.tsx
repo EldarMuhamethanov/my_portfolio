bd454bcf5b17d8331b12f557144e13ef65e4823f
e.muhamethanov <eldarmu.1m@gmail.com>
2024-11-01 18:03:25 +0300

    Правки по дизайну

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react'
 import { Moon, Sun } from 'lucide-react'
-
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 
 export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<'light' | 'dark' | 'system'>(
-    'light',
-  )
+  const [theme, setThemeState] = React.useState<'light' | 'dark'>('light')
 
   React.useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains('dark')
@@ -20,38 +11,24 @@ export function ModeToggle() {
   }, [])
 
   React.useEffect(() => {
-    const isDark =
-      theme === 'dark' ||
-      (theme === 'system' &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    const isDark = theme === 'dark'
     document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
   }, [theme])
 
-  const onChangeTheme = (theme: any) => {
+  const onChangeTheme = (theme: 'light' | 'dark') => {
     setThemeState(theme)
     localStorage.setItem('theme', theme)
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => onChangeTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => onChangeTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
