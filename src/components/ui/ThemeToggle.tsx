36a631a100c864a815f052ced9f04a5abe137c18
e.muhamethanov <eldarmu.1m@gmail.com>
2024-11-05 00:02:16 +0300

    Поправил адаптивность

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -28,6 +28,7 @@ export function ModeToggle() {
       variant="outline"
       size="icon"
       onClick={() => onChangeTheme(theme === 'light' ? 'dark' : 'light')}
+      className="shrink-0"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
